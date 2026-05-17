da7eb2b98be443e74c27dce9a8c2d5d931379edd
Khalid Lamachi <khalidlamachi2005@gmail.com>
2026-05-17 22:13:16 +0100

    Add login page

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
       </Routes>
     </BrowserRouter>
     </React.StrictMode>
